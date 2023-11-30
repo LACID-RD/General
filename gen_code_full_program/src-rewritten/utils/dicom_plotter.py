a8daa8928d9205423adcae231e0f7da7493055c1
RodrigoAlcala <rodrigoalcala00@yahoo.com>
2023-11-30 16:36:46 -0300

    refactir of the plotter

--- gen_code_full_program/src-rewritten/utils/dicom_plotter.py
+++ gen_code_full_program/src-rewritten/utils/dicom_plotter.py
@@ -12,7 +12,7 @@ class DicomPlotter:
         self.dicom_loader = DicomLoaderMRI(directory_path)
         self.dicom_files = self.dicom_loader.sorted_files
 
-    def _show_image(self, arr: np.ndarray):
+    def _show_image(self, arr: np.ndarray, show: bool = True):
         """
         Plot a 2D or 3D array using SimpleITK.
         Requires Fiji or some other external viewer.
@@ -21,21 +21,27 @@ class DicomPlotter:
             arr (np.ndarray): A 2D or 3D array.
 
         Returns:
-            None
+            matplotlib.image.AxesImage: The plot object representing the image.
         """
+        plt_images = []
         itk_image = sitk.GetImageFromArray(arr)
-        
-        #sitk.Show(itk_image)
-        plt.imshow(sitk.GetArrayViewFromImage(itk_image), cmap="gray")
-        plt.show()
+        plt_images.append(plt.imshow(sitk.GetArrayViewFromImage(itk_image), cmap="gray"))
+        print(plt_images)
+        if show:
+            plt.show()
+        return plt_images
 
+    @property
     def _plot_all_files(self):
-        for dicom in self.dicom_files:
-            ds = pydicom.dcmread(dicom)
-            self._show_image(ds.pixel_array)
+        def load_and_plot_images():
+            for dicom in self.dicom_files:
+                ds = pydicom.dcmread(dicom)
+                yield ds.pixel_array
 
-    def _plot_sigle_file(self):
-        img_num_input: int = int(input("Enter img number to plot: "))
+        for pixel_array in load_and_plot_images():
+            self._show_image(pixel_array)
+
+    def _plot_single_file(self, img_num_input: int):
         ds = pydicom.dcmread(self.dicom_files[img_num_input])
         self._show_image(ds.pixel_array)
 
@@ -43,7 +49,7 @@ class DicomPlotter:
 if __name__ == "__main__":
     directory = "/home/ralcala/Documents/AID4ID/testPatients/23342540/20231102/columna_lumbar_2_bl/t2w_tse_sag"
     plotter = DicomPlotter(directory)
-    plotter._plot_all_files()
-    plotter._plot_sigle_file()
-
-    
+    plotter._plot_all_files
+    plotter._plot_single_file(img_num_input=0)
+    # arr = plotter.dicom_loader.volumetric_array
+    # plotter._show_image(arr)
