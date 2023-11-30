b7d11c1b8307860082b2b2779701f60d8f8c5a55
RodrigoAlcala <rodrigoalcala00@yahoo.com>
2023-11-30 16:20:36 -0300

    plotter implementation

--- gen_code_full_program/src-rewritten/utils/dicom_plotter.py
+++ gen_code_full_program/src-rewritten/utils/dicom_plotter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pydicom
 import SimpleITK as sitk
+import matplotlib.pyplot as plt
 
 from dicom_loader import DicomLoaderMRI
 
@@ -9,7 +10,7 @@ class DicomPlotter:
     def __init__(self, directory_path: str) -> None:
         self.path = directory_path
         self.dicom_loader = DicomLoaderMRI(directory_path)
-        self.dicom_files = self.dicom_loader._sorted_files
+        self.dicom_files = self.dicom_loader.sorted_files
 
     def _show_image(self, arr: np.ndarray):
         """
@@ -23,15 +24,18 @@ class DicomPlotter:
             None
         """
         itk_image = sitk.GetImageFromArray(arr)
-        return sitk.Show(itk_image)
+        
+        #sitk.Show(itk_image)
+        plt.imshow(sitk.GetArrayViewFromImage(itk_image), cmap="gray")
+        plt.show()
 
     def _plot_all_files(self):
-        for dicom_file in self.dicom_files:
-            ds = pydicom.dcmread(dicom_file)
+        for dicom in self.dicom_files:
+            ds = pydicom.dcmread(dicom)
             self._show_image(ds.pixel_array)
 
     def _plot_sigle_file(self):
-        img_num_input: int = input("Enter img number to plot: ")
+        img_num_input: int = int(input("Enter img number to plot: "))
         ds = pydicom.dcmread(self.dicom_files[img_num_input])
         self._show_image(ds.pixel_array)
 
@@ -40,4 +44,6 @@ if __name__ == "__main__":
     directory = "/home/ralcala/Documents/AID4ID/testPatients/23342540/20231102/columna_lumbar_2_bl/t2w_tse_sag"
     plotter = DicomPlotter(directory)
     plotter._plot_all_files()
-    pass
+    plotter._plot_sigle_file()
+
+    
